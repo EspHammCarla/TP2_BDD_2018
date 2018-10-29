8ddd7bde12357022ba6e83725545a08d1013cfb1
Federico Fierro <federico.fierro.ff@gmail.com>
2018-10-29 15:24:29 -0300

    Insert goles
    Argentina - Alemania (Alemania 2006)

--- Inserción de Goles.sql
+++ Inserción de Goles.sql
@@ -1,12 +1,7 @@
 use DB_MundialesDeFutbol
-insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
-values ()
-go
+/*Hay que Verificar el timestamp como PK no deja insertar campos Timestamp son generados automaticamente */
 
-insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
-values ()
-go
-
-insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
-values ()
+insert into Gol(min_seg,nro_partido,aÃ±o_mundial,id_tipoGol,pasaporte_jug,en_contra)
+values (49,2,2006,2,28672232,'no'),(80,2,2006,2,99000124,'no'),(91,2,2006,4,99458124,'no'),(93,2,2006,4,99458459,'no'),(95,2,2006,4,98654592,'no')
+,(97,2,2006,4,98125678,'no'),(92,9,2006,4,30147852,'no'),(94,9,2006,4,303459761,'no')
 go
