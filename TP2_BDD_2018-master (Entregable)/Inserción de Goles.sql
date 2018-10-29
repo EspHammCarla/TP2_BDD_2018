b772b99ab686c2450ea4b9b074cd6a1a3c91c842
Federico Fierro <federico.fierro.ff@gmail.com>
2018-10-29 16:01:33 -0300

    Entregable
    Goles Argentina - Alemania (Alemania 2006)

--- TP2_BDD_2018-master (Entregable)/Inserción de Goles.sql
+++ TP2_BDD_2018-master (Entregable)/Inserción de Goles.sql
@@ -1,28 +1,60 @@
 use DB_MundialesDeFutbol
-insert into Gol(min_seg,nro_partido,a�o_mundial,id_tipoGol,pasaporte_jug,en_contra)
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
 values ('00:05:14',1,2006,3,31516745,'no')
 go
 
-insert into Gol(min_seg,nro_partido,a�o_mundial,id_tipoGol,pasaporte_jug,en_contra)
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
 values ('00:14:25',2,2006,3,25485613,'si')
 go
 
-insert into Gol(min_seg,nro_partido,a�o_mundial,id_tipoGol,pasaporte_jug,en_contra)
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
 values ('00:30:24',3,2006,2,25485613,'no')
 go
 
-insert into Gol(min_seg,nro_partido,a�o_mundial,id_tipoGol,pasaporte_jug,en_contra)
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
 values ('00:33:54',4,1978,4,30156485,'no')
 go
 
-insert into Gol(min_seg,nro_partido,a�o_mundial,id_tipoGol,pasaporte_jug,en_contra)
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
 values ('00:43:46',5,2014,5,39879305,'no')
 go
 
-insert into Gol(min_seg,nro_partido,a�o_mundial,id_tipoGol,pasaporte_jug,en_contra)
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
 values ('00:55:13',5,2014,2,39879305,'no')
 go
 
-insert into Gol(min_seg,nro_partido,a�o_mundial,id_tipoGol,pasaporte_jug,en_contra)
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
 values ('01:07:51',5,2014,5,39879305,'no')
+go
+
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
+values ('00:49:25',2,2006,2,28672232,'no')
+go
+
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
+values ('00:80:10',2,2006,2,99000124,'no')
+go
+
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
+values ('00:95:01',2,2006,4,99458124,'no')
+go 
+
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
+values ('00:97:80',2,2006,4,99458459,'no')
+go
+
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
+values ('00:99:00',2,2006,4,98654592,'no')
+go
+
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
+values ('00:99:50',2,2006,4,98125678,'no')
+go
+
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
+values ('00:96:32',2,2006,4,30147852,'no')
+go
+
+insert into Gol(min_seg,nro_partido,año_mundial,id_tipoGol,pasaporte_jug,en_contra)
+values('00:98:01',2,2006,4,303459761,'no')
 go
