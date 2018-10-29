c1597177b68a741e48f5f17f6df6967f2b64dfa7
Federico Fierro <federico.fierro.ff@gmail.com>
2018-10-29 15:19:39 -0300

    Relacion Jugadores y equipos
    Para Partido Argentina Alemania (Alemania 2006)

--- Inserción de Juega_en.sql
+++ Inserción de Juega_en.sql
@@ -1,12 +1,44 @@
 use DB_MundialesDeFutbol
-insert into Juega_en(pasaporte_jug,cod_pais,a�o_mundial,nro_camiseta,es_titular)
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
 values(25485613,9,2006,9,'si')
 go
 
-insert into Juega_en(pasaporte_jug,cod_pais,a�o_mundial,nro_camiseta,es_titular)
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
 values(31516745,25,2014,7,'si')
 go
 
-insert into Juega_en(pasaporte_jug,cod_pais,a�o_mundial,nro_camiseta,es_titular)
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
 values(25486159,5,1978,10,'si')
+go
+
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
+values(28672232,9,2006,2,'si')
+go
+
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
+values(99000124,1,2006,11,'si')
+go
+
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
+values(99458124,1,2006,10,'no')
+go
+
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
+values(99458459,1,2006,13,'si')
+go
+
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
+values(98654592,1,2006,20,'si')
+go
+
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
+values(98125678,1,2006,18,'no')
+go
+
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
+values(30147852,9,2006,2,'no')
+go
+
+insert into Juega_en(pasaporte_jug,cod_pais,año_mundial,nro_camiseta,es_titular)
+values(303459761,9,2006,2,'si')
 go
